d13143039b743efdc575d24ac6e4ec4a9dae2093
trungnt <trung2kgl@gmail.com>
2024-07-14 01:22:42 +0700

    update db

--- Src/Infrastructure/QuickServe.Infrastructure.Persistence/Migrations/20240713182134_UpdateOrder3time.Designer.cs
+++ Src/Infrastructure/QuickServe.Infrastructure.Persistence/Migrations/20240713182134_UpdateOrder3time.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using QuickServe.Infrastructure.Persistence.Contexts;
@@ -11,9 +12,11 @@ using QuickServe.Infrastructure.Persistence.Contexts;
 namespace QuickServe.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240713182134_UpdateOrder3time")]
+    partial class UpdateOrder3time
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
